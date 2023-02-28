ca9dc674b1b6a827178a34f6108c6b6ca11926c6
kerim <yoatomadd@gmail.com>
2023-02-28 12:39:40 +0100

    Added index file - construction + refactoring

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/about', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+    return render_template('index.html')
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
     return render_template('about.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
