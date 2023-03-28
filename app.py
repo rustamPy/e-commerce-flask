b67b136643202ea9a17732494c0bfd5c53789a81
kerim <kerimov.rustam@live.ru>
2023-03-29 00:53:47 +0200

    updated connection

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+import urllib
+
 from flask import Flask, render_template
 
 import pyodbc
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 conn_str = "DRIVER={SQL Server}; SERVER=forkoffee.database.windows.net; DATABASE=mydb;UID=drmohm@forkoffee; PWD=Ferum@1313"
-conn = pyodbc.connect(conn_str)
+params = urllib.parse.quote_plus(conn_str)
 
-cursor = conn.cursor()
+# initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+# extensions
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
