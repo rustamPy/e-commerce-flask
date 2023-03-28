39cd349b8f603278c477ddcc32c63b4a7d52f3cd
kerim <kerimov.rustam@live.ru>
2023-03-29 00:46:17 +0200

    Added connection

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 
+import pyodbc
+
 app = Flask(__name__)
+
+conn_str = "DRIVER={SQL Server}; SERVER=forkoffee.database.windows.net; DATABASE=mydb;UID=drmohm@forkoffee; PWD=Ferum@1313"
+conn = pyodbc.connect(conn_str)
+
+cursor = conn.cursor()
 
 
 @app.route('/', methods=['GET'])
