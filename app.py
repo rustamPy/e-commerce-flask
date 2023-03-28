8d07bf5f54eedbd81869dd5fff521dd493cefa65
kerim <kerimov.rustam@live.ru>
2023-03-29 00:36:54 +0200

    simple

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import os
-from collections import defaultdict
-from database_funcs import add_coffee_to_cart, get_cart_ls, cursor, conn, get_coffee_ls
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
